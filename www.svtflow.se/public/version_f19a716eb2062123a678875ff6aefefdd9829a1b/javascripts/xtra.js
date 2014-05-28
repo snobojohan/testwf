bfe9ecd4cb9949285a8f4c4017c3a870ef7f8b8d
Johan Hallberg <johan.hallberg@gmail.com>
2014-05-28 02:14:09 +0200

    ajax load

--- www.svtflow.se/public/version_f19a716eb2062123a678875ff6aefefdd9829a1b/javascripts/xtra.js
+++ www.svtflow.se/public/version_f19a716eb2062123a678875ff6aefefdd9829a1b/javascripts/xtra.js
@@ -2,5 +2,11 @@ var xtra = {
 	loadInfo: function(obj){
 		console.log("HELLO", obj.find("a").text() );
 		// load to ajaxtarget
+		// /patternlab-node/public/patterns/04-pages-03-titlepage/04-pages-03-titlepage.html
+
+		$.get( "/patternlab-node/public/patterns/04-pages-03-titlepage/04-pages-03-titlepage.html", function( data ) {
+			$( "#ajaxtarget" ).html( data );
+			alert( "Load was performed." );
+		});
 	}
 }
